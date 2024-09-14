34a814005c5ec8e5753ca0a77858a882ecbbd70a
sherrin <sherrinreji1998@gmail.com>
2024-09-14 18:59:18 +0530

    First

---
+++ src/componets/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <div className='bg-blue-400 text-black text-center w-full py-4 mb-2' >
+        <h1 className='text-sans text-2xl'>Tip Calculator</h1>
+    </div>
+    </>
+  )
+}
